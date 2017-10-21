cb59d8cd0ef22a0e9923020e924a1811095fb6a0
Janek Rahrt <janek.rahrt@me.com>
2017-10-21 19:47:50 +0200

    node_env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const isExport = process.env.NODE_ENV === 'export'
+
 module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' }
     }
   },
-  assetPrefix: './'
+  assetPrefix: isExport ? './' : ''
 }
