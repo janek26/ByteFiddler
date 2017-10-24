1d44c484ac488238b035ef0a0f5eaa27f1a6007f
Daniel Opalla <daniel.opalla@gmail.com>
2017-10-24 14:14:03 +0200

    Copy pure html

--- pages/logical-operations.js
+++ pages/logical-operations.js
@@ -4,7 +4,145 @@ export default class LogicalOperationsPage extends Component {
 
   render () {
     return (
-      <div>Hello World</div>
+      		<div id="site">
+			<h3 class="caption">Operand 1</h3>
+			<div id="group1">
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+			</div>
+			
+			
+			<div class="operands">
+				<button id="or">or</button>
+				<button id="and">and</button>
+				<button id="xor">xor</button>
+				<button id="xnor">xnor</button>
+				
+				<button id="add">add</button>
+				<button id="sub">sub</button>
+				<button id="mul">mul</button>
+				<button id="div">div</button>
+			</div>
+			
+			
+			<h3 class="caption">Operand 2</h3>
+			<div id="group2">
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+					<button class="set"></button>
+				</span>
+			</div>
+					
+					
+			<h3 class="caption">Result</h3>
+			<div id="result">
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+				
+				<span class="numGroup">
+					<div class="numbers">0</div>
+				</span>
+			</div>
+
+		</div>
+
     )
   }
 }
