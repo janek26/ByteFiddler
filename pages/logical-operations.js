7f2ff9d929c2e96a91c24dbfcbf77732fa98649a
Daniel Opalla <daniel.opalla@gmail.com>
2017-10-24 14:44:56 +0200

    reformating and copy pure css for jsx

--- pages/logical-operations.js
+++ pages/logical-operations.js
@@ -4,144 +4,198 @@ export default class LogicalOperationsPage extends Component {
 
   render () {
     return (
-      		<div id="site">
-			<h3 class="caption">Operand 1</h3>
-			<div id="group1">
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-			</div>
+		<div id="site">
+		
+    <style jsx>{`
+			h3.caption {
+				margin-bottom: 0px;
+				width: 317px;
+				font-size: 1em;
+				text-align: center;
+			}	
 			
+			#site {
+				width: 900px;
+				height: 700px;
+			}
 			
-			<div class="operands">
-				<button id="or">or</button>
-				<button id="and">and</button>
-				<button id="xor">xor</button>
-				<button id="xnor">xnor</button>
-				
-				<button id="add">add</button>
-				<button id="sub">sub</button>
-				<button id="mul">mul</button>
-				<button id="div">div</button>
-			</div>
-			
-			
-			<h3 class="caption">Operand 2</h3>
-			<div id="group2">
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-					<button class="set"></button>
-				</span>
-			</div>
+			#group1, #group2, #result {
+				width: 315px;
+				height: 60px;
+				border: 1px solid black;
 					
-					
-			<h3 class="caption">Result</h3>
-			<div id="result">
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
+			}
+			
+			#group1 {
+				margin: 0 0 10px 0;
+			}
+			
+			#group2 {
+				margin: 0;
+			}
+			
+			#result {
+				height: 40px;
+			}
+		
+			button.set {
+				width: 20px;
+				height: 15px;
+				margin: 5px 5px;
+			}
 				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-				
-				<span class="numGroup">
-					<div class="numbers">0</div>
-				</span>
-			</div>
-
+			.numbers {    
+				width: 100%;
+				height: 20px;
+				border: 1px solid black;
+				padding: 0 10px;
+			}
+		
+			.numGroup {
+				margin: 8px 13px;
+				float: left;
+				width: 10px;
+			}
+    `}</style>
+		
+		
+		<h3 class="caption">Operand 1</h3>
+		<div id="group1">
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
 		</div>
+		
+		
+		<div class="operands">
+			<button id="or">or</button>
+			<button id="and">and</button>
+			<button id="xor">xor</button>
+			<button id="xnor">xnor</button>
+			
+			<button id="add">add</button>
+			<button id="sub">sub</button>
+			<button id="mul">mul</button>
+			<button id="div">div</button>
+		</div>
+		
+		
+		<h3 class="caption">Operand 2</h3>
+		<div id="group2">
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+				<button class="set"></button>
+			</span>
+		</div>
+				
+				
+		<h3 class="caption">Result</h3>
+		<div id="result">
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+			
+			<span class="numGroup">
+				<div class="numbers">0</div>
+			</span>
+		</div>
+
+	</div>
 
     )
   }
