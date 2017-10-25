27dff5c3adfc45e1e0b57f075ccaa579c2acddd0
Daniel Opalla <daniel.opalla@gmail.com>
2017-10-25 09:32:43 +0200

    init-state for bitdisplay

--- pages/logical-operations.js
+++ pages/logical-operations.js
@@ -11,14 +11,14 @@ export default class LogicalOperationsPage extends Component {
       <LogicalOperations />
       <h3>Operand 1</h3>
       <BitDisplay
-        digits={[7, 6, 5, 4, 3, 2, 1, 0]}
+        digits={[0, 0, 0, 0, 0, 0, 0, 0]}
         fixedNumberOfBits={8}
         id="firstBitDisplay"
       />
 
       <h3>Operand 2</h3>
       <BitDisplay
-        digits={[7, 6, 5, 4, 3, 2, 1, 0]}
+        digits={[0, 0, 0, 0, 0, 0, 0, 0]}
         fixedNumberOfBits={8}
         id="secondBitDisplay"
       />
@@ -26,7 +26,7 @@ export default class LogicalOperationsPage extends Component {
 
       <h3>Result</h3>
       <BitDisplay
-        digits={[7, 6, 5, 4, 3, 2, 1, 0]}
+        digits={[0, 0, 0, 0, 0, 0, 0, 0]}
         fixedNumberOfBits={8}
         id="resultBitDisplay"
       />
