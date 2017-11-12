417f47eff067ef876f9de0060da58fd611f0a23e
LllllllIang <he.lianginemden@gmail.com>
2017-11-12 16:21:54 +0100

    Add files via upload

---
+++ components/Infoanzeige/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import Infoanzeige from './Infoanzeige';
+
+ReactDOM.render(<Infoanzeige />, document.getElementById('root'));
