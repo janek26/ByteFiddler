f6b95ede566c06464b8e2bd7e375c7ced4645b8e
LllllllIang <he.lianginemden@gmail.com>
2017-11-19 20:16:37 +0100

    Delete index.js

--- components/Infoanzeige/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'
-import Infoanzeige from './Infoanzeige';
-
-ReactDOM.render(<Infoanzeige />, document.getElementById('root'));
