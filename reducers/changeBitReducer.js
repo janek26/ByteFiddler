e90f8a588276c84dbcc41e0349f263511de0cd56
Daniel Opalla <Daniel.Opalla@gmail.com>
2017-11-20 14:36:09 +0100

    Cognitive Complexity under 6

--- reducers/changeBitReducer.js
+++ reducers/changeBitReducer.js
@@ -1,12 +1,22 @@
+
+function changePosition(bitGroup, index) {
+  if (bitGroup[index] == 0)
+    bitGroup[index] = 1;
+  else
+    bitGroup[index] = 0;
+    
+  return bitGroup;
+}
+
 function changeBit(state, action) {
   let group = action.payload[0];
   let index = action.payload[1];
 
   if (group == "first")
-    state.bits0[index] = state.bits0[index] == 0 ? 1 : 0;
+    state.bits0 = changePosition(state.bits0, index);
   else if (group == "second")
-    state.bits1[index] = state.bits1[index] == 0 ? 1 : 0;
-    
+    state.bits1 = changePosition(state.bits1, index);
+
   console.log("Group: " + group + ", Bit: " + index);
   console.log(state)
   return  Object.assign({},state,);
