5e410e4296068b70130472a2b70bdb67bcd769cb
Daniel Opalla <Daniel.Opalla@gmail.com>
2017-11-24 17:50:50 +0100

    Remove old Page

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
     return {
       '/': { page: '/' },
       '/logicalPage': { page: '/logicalPage' },
-      '/logical-operations': { page: '/logical-operations' }
     }
   },
   assetPrefix: isExport ? './' : ''
