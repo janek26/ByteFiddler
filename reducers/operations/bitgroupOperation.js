18ec97f2282f08a7d26459f3a191540d1c6e596e
Daniel Opalla <Daniel.Opalla@gmail.com>
2018-01-03 11:37:51 +0100

    carry-bit beim shiften behalten

--- reducers/operations/bitgroupOperation.js
+++ reducers/operations/bitgroupOperation.js
@@ -15,11 +15,19 @@ function bitgroupOperation(state, action) {
     bits = bits.map((bit, index) =>
       bit = bits[index+1]
     ).map((bit, index) => index === 7 ? 0 : bit);
+
+    let oldCarry = state.flags.carry;
+    state.flags = setFlags(bits);
+    state.flags.carry = oldCarry;
   } else if (op == "SHR") {
     state.flags.carry = bits[7];
     bits = bits.map((bit, index) =>
       bit = bits[index-1]
     ).map((bit, index) => index === 0 ? 0 : bit);
+
+    let oldCarry = state.flags.carry;
+    state.flags = setFlags(bits);
+    state.flags.carry = oldCarry;
   } else if (op == "NOT") {
     bits = bits.map((bit, index) =>
       bit === 0
@@ -28,19 +36,26 @@ function bitgroupOperation(state, action) {
     decVal = bin2dec(bits);
     decVal++;
     bits = dec2bin(decVal);
+    let oldCarry = state.flags.carry;
+    state.flags = setFlags(bits);
+    state.flags.carry = oldCarry;
   } else if (op == "DEC") {
     decVal = bin2dec(bits);
     decVal--;
     bits = dec2bin(decVal);
+    setFlags(bits);
+    let oldCarry = state.flags.carry;
+    state.flags = setFlags(bits);
+    state.flags.carry = oldCarry;
   }
 
     if (group === "first") {
       state.bits0 = bits;
-      state.decValues.bits0 = bin2dec(state.bits0);
+        state.decValues.bits0 = bin2dec(state.bits0);
       state.activeGroup = 1;
     } else {
       state.bits1 = bits;
-      state.decValues.bits1 = bin2dec(state.bits1);
+        state.decValues.bits1 = bin2dec(state.bits1);
       state.activeGroup = 2;
     }
 
